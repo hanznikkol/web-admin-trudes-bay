11e9c6ea2b15dec621876097dd81fcc92e96d26d
Hanz <hanznikkolmaas@gmail.com>
2024-11-24 15:39:06 +0800

    Admin Responsiveness

--- AdminReservationRoomList.php
+++ AdminReservationRoomList.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trudes Bay Admin Reservation List</title>
-    <link rel="stylesheet" href="AdminReservationsLists-style.css">
+    <link rel="stylesheet" href="AdminReservationsLists-style.css?ver=<?php echo time();?>">
     <link rel="website icon" type="png" href="Images/Trudes Bay_Final.png" />
 
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
@@ -12,7 +12,28 @@
 <body>
     <header>
         <img src="Images/Trudes Bay Strip logo.png" alt="Trudes Bay Beach Resort">
+        <svg class="svg-hamburger" onclick="toggleNavigation()" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M7.5 42.5H52.5M7.5 30H52.5M7.5 17.5H52.5" stroke="white" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        <script>
+            function toggleNavigation() {
+                const navMenu = document.querySelector('nav'); // Select the nav element
+                navMenu.classList.toggle('active'); // Toggle the 'active' class
+            }
+        </script>
     </header>
+
+    <!--Show only in mobile-->
+    <nav class="mobile-nav">
+        <a href="AdminAccount.php"><i class='bx bxs-user-account'></i> <span class="nav-item">Accounts</span></a>
+        <a href="AdminIndex.php"><i class='bx bxs-home-circle'></i> <span class="nav-item">Home</span></a>
+        <a href="AdminReservationList.php"><i class='bx bxs-book-content'></i> <span class="nav-item">Reservation</span></a>
+        <a href="AdminAmenities.php"><i class='bx bxs-image'></i> <span class="nav-item">Amenities</span></a>
+        <a href="AdminActivities.php"><i class='bx bx-swim'></i> <span class="nav-item">Activities</span></a>
+        <a href="AdminAbout.php"><i class='bx bxs-news'></i> <span class="nav-item">About</span></a>
+        <a href="AdminFAQ.php"><i class='bx bx-help-circle'></i> <span class="nav-item">FAQ's</span></a>
+        <a href="#"><i class="bx bxs-log-out"></i> <span class="nav-item">Logout</span></a>
+    </nav>
 
     <div class="sidebar">
         <div class="top">
